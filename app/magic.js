5b40bfc82b4adcb9bfe4330c00edf339d8ac1ce2
djedynak259 <dajed@outlook.com>
2017-01-25 22:29:33 -0800

    tsting2

--- app/magic.js
+++ app/magic.js
@@ -1,5 +1,17 @@
 var message = 'hello';
 function saySomething(message) {
+    if (message === void 0) { message = 'whats up'; }
     console.log(message);
 }
 saySometing(message);
+var Greeter = (function () {
+    function Greeter() {
+    }
+    Greeter.prototype.saySomething = function (message) {
+        if (message === void 0) { message = 'hi'; }
+        return message + 'something';
+    };
+    return Greeter;
+}());
+var greeter = new Greeter();
+greeter.saySomething('hello');
--- app/magic.ts
+++ app/magic.ts
@@ -1,7 +1,18 @@
 var message = 'hello';
 
-function saySomething(message: string) {
+function saySomething(message: string = 'whats up') {
 	console.log(message);
 }
 
 saySometing(message);
+
+
+class Greeter {
+	
+	saySomething(message: string = 'hi'){
+		return message + 'something';
+	}
+}
+
+var greeter = new Greeter();
+greeter.saySomething('hello');
